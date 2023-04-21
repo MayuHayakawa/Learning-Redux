e6fada75d868f2c00fc7ef4f7b9850f2e308d969
Mayu <mayu.i.19930327@gmail.com>
2023-04-21 14:53:17 -0700

    added another way

--- cart-app-withToolkit/src/components/Cart/Cart.jsx
+++ cart-app-withToolkit/src/components/Cart/Cart.jsx
@@ -1,51 +1,117 @@
+import { useState } from 'react';
 import { addItem, removeItem } from './cartSlice';
 import { useDispatch, useSelector } from 'react-redux';
-import { useRef } from "react";
-import { v4 as uuidv4 } from "uuid";
-import styled from 'styled-components';
+// import { useRef } from "react";
+// import { v4 as uuidv4 } from "uuid";
+// import styled from 'styled-components';
 
-const ItemListContainer = styled.div`
-    display: flex;
-`
+// const ItemListContainer = styled.div`
+//     display: flex;
+// `
 
 const Cart = () => {
     const dispatch = useDispatch();
-    const items = useSelector((state) => state.cart.items);
-    const useRefItem = useRef(null);
-    const useRefPrice = useRef(null);
-    
-    const handleAdd = () => {
-        let item = {
-            id: uuidv4(),
-            name: useRefItem.current.value,
-            price: useRefPrice.current.value
-        }
-        dispatch(addItem(item));
-        useRefItem.current.value = "";
-        useRefPrice.current.value = "";
+    const cart = useSelector((state) => state.cart.items);
+    const [ data, setData ] = useState({
+        id: "",
+        name: "",
+        price: ""
+    });
+
+    const handleRemove = (id) => {
+        dispatch(removeItem(id));
     }
 
-    console.log(items);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
 
-  return (
-    <div>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(addItem(data));
+        setData({
+            id: "",
+            name: "",
+            price: ""
+        });
+    };
+
+    return(
         <div>
-            <input type="text" placeholder='enter item name' ref={useRefItem}/>
-            <input type="number" placeholder="enter item price" ref={useRefPrice} />
-            <button onClick={() => handleAdd()}>Add item</button>
+            {cart.map((item) => (
+                <div key={item.id}>
+                    <h5>{item.name}</h5>
+                    <button onClick={() => handleRemove(item.id)}>Delete</button>
+                </div>
+            ))}
+            <form onSubmit={handleSubmit}>
+                <input 
+                    onChange={handleChange}
+                    value={data.id}
+                    type="text"
+                    name="id"
+                    id="itemId"
+                    />
+                <input 
+                    onChange={handleChange}
+                    value={data.name}
+                    type="text"
+                    name="name"
+                    id="itemName"
+                />
+                <input 
+                    onChange={handleChange}
+                    value={data.price}
+                    type="number"
+                    name="price"
+                    id="itemPrice"
+                />
+                <button>Submit</button>
+            </form>
         </div>
-        <div>
-            {items.map(item => {
-                return(
-                    <ItemListContainer key={item.id}>
-                        <h3>{item.name} - {item.price}</h3>
-                        <button onClick={() => dispatch(removeItem(item.id))}>remove</button>
-                    </ItemListContainer>
-                )
-            })}
-        </div>
-    </div>
-  )
+
+    )
+//     const dispatch = useDispatch();
+//     const items = useSelector((state) => state.cart.items);
+//     const useRefItem = useRef(null);
+//     const useRefPrice = useRef(null);
+    
+//     const handleAdd = () => {
+//         let item = {
+//             id: uuidv4(),
+//             name: useRefItem.current.value,
+//             price: useRefPrice.current.value
+//         }
+//         dispatch(addItem(item));
+//         useRefItem.current.value = "";
+//         useRefPrice.current.value = "";
+//     }
+
+//     console.log(items);
+
+//   return (
+//     <div>
+//         <div>
+//             <input type="text" placeholder='enter item name' ref={useRefItem}/>
+//             <input type="number" placeholder="enter item price" ref={useRefPrice} />
+//             <button onClick={() => handleAdd()}>Add item</button>
+//         </div>
+//         <div>
+//             {items.map(item => {
+//                 return(
+//                     <ItemListContainer key={item.id}>
+//                         <h3>{item.name} - {item.price}</h3>
+//                         <button onClick={() => dispatch(removeItem(item.id))}>remove</button>
+//                     </ItemListContainer>
+//                 )
+//             })}
+//         </div>
+//     </div>
+//   )
 }
 
 export default Cart
--- cart-app-withToolkit/src/components/Cart/cartSlice.js
+++ cart-app-withToolkit/src/components/Cart/cartSlice.js
@@ -3,20 +3,29 @@ import { createSlice } from '@reduxjs/toolkit';
 const cartSlice = createSlice({
     name: "cart",
     initialState: {
-        items: [],
-        item: {
-            id: "",
-            name: "",
-            price: 0
-        }
+        items: [
+            {
+                id: 1,
+                name: "Iphone 1",
+                price: 1000
+            }
+        ]
+        // items: [],
+        // item: {
+        //     id: "",
+        //     name: "",
+        //     price: 0
+        // }
     },
     reducers: {
         addItem: (state, action) => {
-            if(state.items.length === 0) {
-                state.items[0] = action.payload
-            } else {
-                state.items = [...state.items, action.payload];
-            }
+            console.log(state.items);
+            state.items.push(action.payload);
+            // if(state.items.length === 0) {
+            //     state.items[0] = action.payload
+            // } else {
+            //     state.items = [...state.items, action.payload];
+            // }
         },
         removeItem: (state, action) => {
             state.items = state.items.filter(item => item.id != action.payload);
